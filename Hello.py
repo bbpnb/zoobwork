5ec3f3b49d928ab9227a84b49362e6c8f5908848
bbpnb <yuanzb@gmail.com>
2023-12-15 15:33:03 +0000

    comm2

--- Hello.py
+++ Hello.py
@@ -27,8 +27,7 @@ def run():
     )
 
     st.write("# Welcome to Streamlit! 👋")
-/workspaces/zoobwork/pages
-    
+
     df = pd.DataFrame({
       'first column': [1, 2, 3, 4],
       'second column': [10, 20, 30, 40],
@@ -37,25 +36,9 @@ def run():
 
     df
 
+
     st.sidebar.success("Select a demo above.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
 
 if __name__ == "__main__":
